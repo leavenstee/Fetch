a16d5c37753970e797459d5235e04ba0db42a3a8
Steven Lee <stevenleedos@gmail.com>
2018-10-22 07:37:50 -0500

    continued to integrate items

--- Fetch-Example/ViewController.swift
+++ Fetch-Example/ViewController.swift
@@ -8,59 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource {
-    struct Photo: Codable
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    struct ImageObject: Codable
     {
         //String, URL, Bool and Date conform to Codable.
         var author: String
         var author_url: URL
         var filename: String
-        var format
-        //The Dictionary is of type [String:String] and String already conforms to Codable.
-        var metaData: [String:String]
-        
-        //PhotoType and Size are also Codable types
-        var type: PhotoType
-        var size: Size
+        var format : String
+        var height : Int
+        var id : Int
+        var post_url : URL
+        var width: Int
+
     }
     
-    var collectionView : UICollectionView!
-    var data : [ImageObject]!
+    var data : [ImageObject] = []
+    
+    lazy var collectionView: UICollectionView = {
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        setupCollectionView()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        view.addSubview(self.collectionView)
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[collectionView]-0-|", options: [], metrics: nil, views: ["collectionView":collectionView]))
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[collectionView]-0-|", options: [], metrics: nil, views: ["collectionView":collectionView]))
+        
+        loadData()
   
     }
-    
-    func setupCollectionView() {
-        collectionView = UICollectionView(frame: .zero)
-        collectionView.collectionViewLayout = .init()
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(self.collectionView)
-    }
-    
+
     
     func loadData() {
         let fetcher = Fetch.sharedFetcher
         fetcher.get(urlString: "https://picsum.photos/list", params: nil) { (json) in
-            
+            let photoArrayData = json as! [Dictionary<String, Any>]
+            for photoData in photoArrayData {
+                self.data.append(ImageObject(author: photoData["author"] as! String,
+                                        author_url: URL(string: photoData["author_url"] as! String)!,
+                                       filename: photoData["filename"] as! String,
+                                       format: photoData["format"] as! String,
+                                       height: photoData["height"] as! Int,
+                                       id: photoData["id"] as! Int,
+                                       post_url: URL(string: photoData["post_url"] as! String)!,
+                                       width:photoData["width"] as! Int))
+            }
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
         }
     }
-}
-
-extension ViewController: UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return data.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
+        
+        cell.backgroundColor = .red
+        
+        print(data[indexPath.row].author)
+        
+        return cell
     }
-    
 }
+
 
 
